15603453904d4885b164c45e6e7e63999d255f7d
Bruno E. Silva <bruno.esilva@gmail.com>
2022-11-11 22:49:08 -0300

    Atualizações no projeto currículo - Criação do currículo Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HomeComponent,
+    NavigationComponent,
 
   ],
   imports: [
